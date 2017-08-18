03e0cc12c41c2326deda63c0ba406703ee5e6c1f
Mickey Yawn <mickeyyawn@gmail.com>
2017-08-18 13:01:35 -0400

    scaffolding and dependencies

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname)
+
+console.log('running at: ' + 'http://' + hostname + ':' + port)
